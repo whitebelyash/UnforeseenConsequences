38453fcabbbe2c43659770f72f07468c04af2e02
WindClan <monstertoast2@gmail.com>
2024-09-01 17:14:44 -0400

    Block calls to enum not present in GL ES

--- src/main/java/org/featherwhisker/rendereng/mixins/GlStateManagerMixin.java
+++ src/main/java/org/featherwhisker/rendereng/mixins/GlStateManagerMixin.java
@@ -1,11 +1,13 @@
 package org.featherwhisker.rendereng.mixins;
 
 import com.mojang.blaze3d.platform.GlStateManager;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.nio.ByteBuffer;
+import static org.lwjgl.opengl.GL14.*;
 
 @Mixin(GlStateManager.class)
 public class GlStateManagerMixin {
@@ -33,4 +35,15 @@ public class GlStateManagerMixin {
      */
     @Overwrite
     public static void _glDrawPixels(int width, int height, int format, int type, long pixels) {}
+
+    @Inject(
+            method = "_texParameter",
+            at=@At("HEAD"),
+            cancellable = true
+    )
+    private static void _glDrawPixels(int a, int b, float c, CallbackInfo call) {
+        if (a == GL_TEXTURE_2D && b == GL_TEXTURE_LOD_BIAS) {
+            call.cancel();
+        }
+    }
 }
