950cc8fc8fc20b9fb852b62c37c82f98bc53a6cd
WindClan <monstertoast2@gmail.com>
2024-10-12 15:53:16 -0400

    a1.2

--- src/main/java/org/featherwhisker/rendereng/main.java
+++ src/main/java/org/featherwhisker/rendereng/main.java
@@ -15,11 +15,12 @@ public class main implements ModInitializer {
     }
 
     public static String convertShader(String str,int i) {
-        String append = "precision lowp float;";
+        String append = "precision mediump float;\nprecision mediump int;\n\n";
         return (str
                 .replaceAll("#version 150","#version "+shaderVersion+"\n"+append)
                 .replaceAll("texCoord2 = UV2;","texCoord2 = vec2(UV2);")
 				.replaceAll("uv / 256.0","vec2(uv) / 256.0")
+                .replaceAll("//.*\\n//.*\\n/.*\\n","") //because CC needs to have comments before the version statement
         );
     }
 }
