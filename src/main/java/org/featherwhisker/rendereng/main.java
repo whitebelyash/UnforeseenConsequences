1858316be06aa6c8275329b96060775b2d97dab2
whitebelyash <66699673+whitebelyash@users.noreply.github.com>
2025-08-18 18:54:21 +0400

    fix(shader_converter): apply 1.21.4 core/lightmap workaround

--- src/main/java/org/featherwhisker/rendereng/main.java
+++ src/main/java/org/featherwhisker/rendereng/main.java
@@ -18,9 +18,14 @@ public class main implements ModInitializer {
         String append = "precision mediump float;\nprecision mediump int;\n\n";
         return (str
                 .replaceAll("#version 150","#version "+shaderVersion+"\n"+append)
-                .replaceAll("texCoord2 = UV2;","texCoord2 = vec2(UV2);")
-				.replaceAll("uv / 256.0","vec2(uv) / 256.0")
-                .replaceAll("//.*\\n//.*\\n/.*\\n","") //because CC needs to have comments before the version statement
+               .replaceAll("texCoord2 = UV2;","texCoord2 = vec2(UV2);")
+			    .replaceAll("uv / 256.0","vec2(uv) / 256.0")
+                // Hacky 1.21.3/4 lightmap workaround - probably there are some proper fixes to this?
+                .replaceAll("float block_brightness = get_brightness\\(floor\\(texCoord.x \\* 16\\) / 15\\) \\* BlockFactor",
+                        "float block_brightness = get_brightness(floor(texCoord.x * 16.0) / 15.0) * BlockFactor")
+                .replaceAll("float sky_brightness = get_brightness\\(floor\\(texCoord.y \\* 16\\) / 15\\) \\* SkyFactor",
+                        "float sky_brightness = get_brightness(floor(texCoord.y * 16.0) / 15.0) * SkyFactor")
+             //   .replaceAll("//.*\\n//.*\\n/.*\\n","") //because CC needs to have comments before the version statement
         );
     }
 }
