46beb08827a8a2391eb7e93acc8c8710edd7dc12
whitebelyash <66699673+whitebelyash@users.noreply.github.com>
2025-08-18 19:00:09 +0400

    fix(mixin): ShaderStageMixin -> CompiledShaderMixin

---
+++ src/main/java/org/featherwhisker/rendereng/mixins/CompiledShaderMixin.java
@@ -0,0 +1,39 @@
+package org.featherwhisker.rendereng.mixins;
+
+import net.minecraft.client.gl.CompiledShader;
+
+import net.minecraft.util.Identifier;
+import org.featherwhisker.rendereng.util.ShaderConverter;
+import org.jetbrains.annotations.NotNull;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import static com.mojang.blaze3d.platform.GlStateManager.glShaderSource;
+import static org.featherwhisker.rendereng.main.*;
+
+@Mixin(CompiledShader.class)
+public class CompiledShaderMixin {
+	@Redirect(
+			method = "compile",
+			at = @At(
+					value = "INVOKE",
+					target = "com/mojang/blaze3d/platform/GlStateManager.glShaderSource (ILjava/lang/String;)V"
+			)
+	)
+	private static void glShaderSourceIntercept(int i, @NotNull String source) {
+		if (shouldConvertShaders) {
+			source = ShaderConverter.convert(source);
+			log.info("Converted source: " + source);
+			glShaderSource(i, source);
+		}
+	}
+	@Inject(method = "compile", at = @At(value = "HEAD"))
+	private static CompiledShader compileHead(Identifier id, CompiledShader.Type type, String source, CallbackInfoReturnable<CompiledShader> info){
+		log.info("Compiling shader: " + id.getNamespace() + ":" + id.getPath());
+		return info.getReturnValue();
+	}
+}
